be354ed9bb9b4b61e0362aa42192628b0531bb15
azevedosadraque <35151827+azevedosadraque@users.noreply.github.com>
2026-03-24 18:06:27 -0300

    remove book and sheet

--- _tmp_pdf_extract/_tmp_bing_search.py
+++
@@ -1,8 +0,0 @@
-import urllib.parse
-import urllib.request
-
-q = 'Magnetismo "Filhos do Éden"'
-url = 'https://www.bing.com/search?q=' + urllib.parse.quote(q)
-with urllib.request.urlopen(url, timeout=20) as r:
-    html = r.read().decode('utf-8', errors='ignore')
-print(html[:20000])
